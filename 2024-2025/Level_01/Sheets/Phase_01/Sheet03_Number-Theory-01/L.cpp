744d5489b9219859f8ab2c99b51537a3a214b2dd
ShwaTech <shwakesh13579@gmail.com>
2026-08-29 20:49:44 +0300

    Sheet03 Number Theory 01 - Problem: L Solved 🎈

---
+++ 2024-2025/Level_01/Sheets/Phase_01/Sheet03_Number-Theory-01/L.cpp
@@ -0,0 +1,39 @@
+// لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+// استغفر الله العظيم الذي لا الاه الا هو الحي القيوم واتوب اليه
+// سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+// سبحان الله وبحمده، سبحان الله العظيم
+// اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+// لا الاه الا الله وحده هو يتولي الصالحين
+// ربي اني ظلمت نفسي، فاغفر لي
+// حسبي الله لا الاه الا هو عليه توكلت وهو رب العرش العظيم
+// بسم الله نبدأ وعليه نتوكل
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define All(v) v.begin(),v.end()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+
+bool Is_Power_of_Two (ll x) {
+    return ((x & (x - 1)) == 0);
+}
+
+
+int main()
+{
+    ShwaTech
+
+    int T; cin >> T;
+    while (T--) {
+        ll n; cin >> n;
+
+        // If n has an odd prime factor, then n is not a power of 2.
+
+        cout << ( !Is_Power_of_Two(n) ? "YES\n" : "NO\n" );
+    }
+
+    return 0;
+}
