f25265b2481627a1adff8c6a8537c86613b377b5
ShwaTech <shwakesh13579@gmail.com>
2026-08-31 12:37:49 +0300

    🎈 Level_01 - Phase_01 - Contest_03 - Problem: A Solved 🎉

---
+++ 2024-2025/Level_01/Contests/Phase01/Contest_03/A.cpp
@@ -0,0 +1,52 @@
+/*
+لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+استغفر الله العظيم الذي لا إلاه إلا هو الحي القيوم وأتوب اليه
+سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+سبحان الله وبحمده، سبحان الله العظيم
+اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+لا إلاه إلا الله وحده هو يتولي الصالحين
+لا إلاه إلا الله وحده هو يهدي السبيل
+ربي إني ظلمت نفسي، فاغفر لي
+ربي إني لما أنزلت اليّ من خيرٍ فقير
+حسبي الله لا إلاه إلا هو عليه توكلت وهو رب العرش العظيم
+بسم الله نبدأ وعليه نتوكل
+*/
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define All(v) v.begin(),v.end()
+#define RAll(v) v.rbegin(),v.rend()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+
+int main()
+{
+    ShwaTech
+
+    int T; cin >> T;
+    while (T--) {
+        int n; cin >> n;
+
+        map<int, int> freq;
+        for (int i = 0; i < n; i++) {
+            int x; cin >> x;
+
+            freq[x]++;
+        }
+
+        vector<int> ans(n+1);
+        for (auto [frog, frq]: freq) {
+            for (int hop = frog; hop <= n; hop += frog) {
+                ans[hop] += frq;
+            }
+        }
+
+        cout << *max_element(All(ans)) << nl;
+    }
+
+    return 0;
+}
+
