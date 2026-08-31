145e2c40595937756fd1e450d41bf77895cf7bf4
ShwaTech <shwakesh13579@gmail.com>
2026-08-31 18:39:41 +0300

    🎈 Level_01 - Phase_01 - Contest_03 - Problem: E Solved 🎉

---
+++ 2024-2025/Level_01/Contests/Phase_01/Contest_03/E.cpp
@@ -0,0 +1,59 @@
+/*
+لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+استغفر الله العظيم الذي لا إلاه إلا هو الحي القيوم وأتوب اليه
+سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+سبحان الله وبحمده، سبحان الله العظيم
+اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+لا إلاه إلا الله وحده هو يتولي الصالحين
+لا إلاه إلا الله وحده هو يهدي السبيل
+ربي إني ظلمت نفسي، فاغفر لي
+ربي إني لما أنزلت اليّ من خيرٍ فقير
+حسبي الله لا إلاه إلا هو عليه توكلت وهو رب العرش العظيم
+بسم الله نبدأ وعليه نتوكل
+*/
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define All(v) v.begin(),v.end()
+#define RAll(v) v.rbegin(),v.rend()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+
+int main()
+{
+    ShwaTech
+
+    string s; cin >> s;
+
+    int ones = count(All(s), '1');
+
+    string ans="";
+
+    bool onesAdded = false;
+
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '1') continue;
+
+        if (s[i] == '0') {
+            ans.push_back('0');
+        } else if (s[i] == '2' && !onesAdded) {
+            ans += string(ones, '1');
+            ans.push_back('2');
+            onesAdded = true;
+        } else if (s[i] == '2') {
+            ans.push_back('2');
+        }
+    }
+
+    if (!onesAdded) {
+        ans += string(ones, '1');
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
