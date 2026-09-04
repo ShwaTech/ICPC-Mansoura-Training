1b2428030ce59dbd853ce474b6e031e06c1c810b
ShwaTech <shwakesh13579@gmail.com>
2026-09-04 21:35:05 +0300

    🎉 Level_01 - Phase_01 - Sheet04_Number_Theory_02 - Problem: B Solved 🎈

---
+++ 2024-2025/Level_01/Sheets/Phase_01/Sheet04_Number-Theory-02/B.cpp
@@ -0,0 +1,52 @@
+/*
+لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+استغفر الله العظيم الذي لا إلاه إلا هو الحي القيوم وأتوب اليه
+سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+سبحان الله وبحمده، سبحان الله العظيم
+اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+لا إلاه إلا الله وحده هو يتولي الصالحين
+لا إلاه إلا الله وحده هو يهدي السبيل
+ربي إني ظلمت نفسي، فاغفر لي
+ربي إني لما أنزلت اليّ من خيرٍ فقير
+حسبي الله لا إلاه إلا هو عليه توكلت وهو رب العرش العظيم
+بسم الله نبدأ وعليه نتوكل
+*/
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define ld long double
+#define All(v) v.begin(),v.end()
+#define RAll(v) v.rbegin(),v.rend()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+
+
+int main()
+{
+    ShwaTech
+
+    string s; ll mod;
+    cin >> s >> mod;
+
+    reverse(All(s));
+
+    ll ans=0, powerTen=1;
+
+    for (ll i = 0; i < s.size(); i++) {
+        ll digit = s[i] - '0';
+
+        ans += (digit % mod * powerTen % mod) % mod;
+        ans %= mod;
+
+        powerTen *= 10 % mod;
+        powerTen %= mod;
+    }
+
+    cout << ans;
+
+    return 0;
+}
+
