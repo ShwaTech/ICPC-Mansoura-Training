ab62561ebbe76b0b2fb93c565b1a2f870e5cbd27
ShwaTech <shwakesh13579@gmail.com>
2026-09-04 22:37:47 +0300

    🎉 Level_01 - Phase_01 - Sheet04_Number_Theory_02 - Problem: D Solved 🎈

---
+++ 2024-2025/Level_01/Sheets/Phase_01/Sheet04_Number-Theory-02/D.cpp
@@ -0,0 +1,59 @@
+/*
+لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+استغفر الله العظيم الذي لا إلاه إلا هو الحي القيوم وأتوب اليه
+سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+سبحان الله وبحمده، سبحان الله العظيم
+اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+لا إلاه إلا الله وحده هو يتولي الصالحين
+لا إلاه إلا الله وحده هو يهدي السبيل
+ربي إني ظلمت نفسي، فاغفر لي
+ربي إني لما أنزلت اليّ من خيرٍ فقير
+حسبي الله لا إلاه إلا هو عليه توكلت وهو رب العرش العظيم
+بسم الله نبدأ وعليه نتوكل
+*/
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define ld long double
+#define All(v) v.begin(),v.end()
+#define RAll(v) v.rbegin(),v.rend()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+const ll mod = 1e9+7;
+
+ll Binary_Exponentiation (ll base, ll power) {
+    ll res = 1;
+
+    while (power) {
+        if (power & 1) {
+            power--;
+            res *= base % mod;
+            res %= mod;
+        } else {
+            power /= 2;
+            base *= base % mod;
+            base %= mod;
+        }
+    }
+
+    return res;
+}
+
+
+int main()
+{
+    ShwaTech
+
+    int T; cin >> T;
+    while (T--) {
+        ll a, b; cin >> a >> b;
+
+        cout << Binary_Exponentiation(a, b) << nl;
+    }
+
+    return 0;
+}
+
