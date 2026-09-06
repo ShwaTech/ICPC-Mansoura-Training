5153850473b5783adc6bd521b8aeee32e5976bd4
ShwaTech <shwakesh13579@gmail.com>
2026-09-06 22:25:49 +0300

    🎉 Level_01 - Phase_01 - Sheet04_Number_Theory_02 - Problem: M Solved 🎈

---
+++ 2024-2025/Level_01/Sheets/Phase_01/Sheet04_Number-Theory-02/M.cpp
@@ -0,0 +1,48 @@
+/*
+لا الاه الا الله وحده لا شريك له له الملك وله الحمد، وهو علي كل شئ قدير
+استغفر الله العظيم الذي لا إلاه إلا هو الحي القيوم وأتوب اليه
+سبحان الله، الحمد لله، لا الاه الا الله، الله اكبر، لا حول ولا قوة الا بالله
+سبحان الله وبحمده، سبحان الله العظيم
+اللهم صلي وسلم وزد وبارك علي عبدك ونبيك محمد
+لا إلاه إلا الله وحده هو يتولي الصالحين
+لا إلاه إلا الله وحده هو يهدي السبيل
+ربي إني ظلمت نفسي، فاغفر لي
+ربي إني لما أنزلت اليّ من خيرٍ فقير
+حسبي الله لا إلاه إلا هو عليه توكلت وهو رب العرش العظيم
+بسم الله نبدأ وعليه نتوكل
+*/
+
+#include <bits/stdc++.h>
+#define nl "\n"
+#define ll long long
+#define ld long double
+#define All(v) v.begin(),v.end()
+#define RAll(v) v.rbegin(),v.rend()
+#define ShwaTech ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+using namespace std;
+
+
+
+int main()
+{
+    ShwaTech
+
+    int T; cin >> T;
+    while (T--) {
+        ll n, x, y; cin >> n >> x >> y;
+
+        ll LCM = x / __gcd(x, y) * y;
+
+        ll X = (n / x) - (n / LCM);
+        ll Y = (n / y) - (n / LCM);
+
+        ll sumY = Y * (Y + 1) / 2;
+        ll sumX = (n * (n + 1) / 2) - ((n - X) * ((n - X) + 1) / 2);
+
+        cout << sumX - sumY << nl;
+    }
+
+    return 0;
+}
+
